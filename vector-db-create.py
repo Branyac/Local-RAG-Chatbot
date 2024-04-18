684068d0319ee55d86fea1ffb6e9bf7534e59c8c
Branyac <1702716+Branyac@users.noreply.github.com>
2024-04-18 23:55:34 +0200

    - Add hability to read all PDFs in a folder - Enable hardware acceleration

--- vector-db-create.py
+++ vector-db-create.py
@@ -1,23 +1,33 @@
-#vector-db-create.py
-# create a vector database from a pdf file
+# Create a vector database from PDF files in ./pdf folder
+import os
 
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
+PATH_TO_PDFS = './pdfs/'
+PATH_TO_DATABASE = './chroma_db_nccn'
+DEVICE = 'cuda:0' # Set to 'cpu' for non-Nvidia GPUs to disable Acceleration
 
-loaders = [PyPDFLoader('./pdfs/brain-gliomas-patient.pdf')]
+loaders = []
+for file in os.listdir(PATH_TO_PDFS):
+    full_path_to_file = f"{PATH_TO_PDFS}{file}"
+    if os.path.isfile(full_path_to_file):
+        print(f'Add file "{full_path_to_file}" for processing')
+        loaders.append(PyPDFLoader(full_path_to_file))
 
 docs = []
 for file in loaders:
+    print(f'Processing "{file.source}"')
     docs.extend(file.load())
+
 #split text to chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(docs)
-embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': DEVICE})
 #print(len(docs))
 
-vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db_nccn")
+vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory=PATH_TO_DATABASE)
 
 print(vectorstore._collection.count())
