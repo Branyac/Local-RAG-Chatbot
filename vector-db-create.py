543fe28c11718cebcf81ed97b7390a3ad4d3c3d4
Branyac <1702716+Branyac@users.noreply.github.com>
2024-04-24 00:30:21 +0200

    - Fix imports - Change embedding model to other more powerful - Change chunk size and overlap - Generate chunks with all the document, not just a page - Simplify code

--- vector-db-create.py
+++ vector-db-create.py
@@ -1,33 +1,38 @@
 # Create a vector database from PDF files in ./pdf folder
 import os
-
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders.pdf import PyPDFLoader
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.documents import Document
+
 
 PATH_TO_PDFS = './pdfs/'
 PATH_TO_DATABASE = './chroma_db_nccn'
 DEVICE = 'cuda:0' # Set to 'cpu' for non-Nvidia GPUs to disable Acceleration
 
-loaders = []
+embedding_function : HuggingFaceEmbeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2", model_kwargs={'device': DEVICE})
 for file in os.listdir(PATH_TO_PDFS):
     full_path_to_file = f"{PATH_TO_PDFS}{file}"
     if os.path.isfile(full_path_to_file):
-        print(f'Add file "{full_path_to_file}" for processing')
-        loaders.append(PyPDFLoader(full_path_to_file))
+        print(f'Processing file "{file}"')
 
-docs = []
-for file in loaders:
-    print(f'Processing "{file.source}"')
-    docs.extend(file.load())
+        print('  - Preparing loader...')
+        loader : PyPDFLoader = PyPDFLoader(full_path_to_file)
+        docs = loader.load()
 
-#split text to chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-docs = text_splitter.split_documents(docs)
-embedding_function = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': DEVICE})
-#print(len(docs))
+        print('  - Loading text...')
+        full_document_text : str = " ".join([doc.page_content for doc in docs])
+        full_document_metadata = {
+                "filename": file
+            }
+        full_document : Document = Document(page_content=full_document_text, metadata=full_document_metadata)
 
-vectorstore = Chroma.from_documents(docs, embedding_function, persist_directory=PATH_TO_DATABASE)
+        print('  - Splitting document...')
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separators=[ "\n\n", "\n" ])
+        splitted_doc = text_splitter.split_documents( [ full_document ])
+        
+        print('  - Creating embbedings...')
+        Chroma.from_documents(documents=splitted_doc, embedding=embedding_function, persist_directory=PATH_TO_DATABASE)
 
-print(vectorstore._collection.count())
+print('End')
